7a66d30c09068dc1c61f39414e1be73dd4c20b7d
MareBogdan <marebogdan03@gmail.com>
2024-12-22 14:36:38 +0200

    Add project files.

---
+++ Mare_Bogdan_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mare_Bogdan_Lab11.Models;
+
+namespace Mare_Bogdan_Lab11.Data
+{
+    internal interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
---
+++ Mare_Bogdan_Lab11/Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mare_Bogdan_Lab11.Models
+{
+    internal class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
